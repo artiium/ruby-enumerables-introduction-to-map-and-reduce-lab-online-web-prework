9878b25d9b2a38c386377d4ff8564b11befe373d
Artiium Arbyummi <artiium21@gmail.com>
2019-09-08 19:53:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,5 +37,12 @@ end
 
 
 def reduce_to_any_true(source_array)
+ counter = 0
+while counter < source_array.size do 
+  return true if source_array[counter] == true
+  counter += 1
+  
+end
+return true 
 end
 
